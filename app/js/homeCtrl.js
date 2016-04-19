09753146ff13317bb420394ecc306e07b091443a
Christ1992 <yingjie@n180-p123.kthopen.kth.se>
2016-04-19 15:21:30 +0200

    add home page

--- app/js/homeCtrl.js
+++ app/js/homeCtrl.js
@@ -2,10 +2,116 @@
 // information for one dish
 
 weatherDressApp.controller('homeCtrl', function ($scope,Weather) {
-    $scope.curWea= Weather.getCurrentWeather();
-    $scope.forWea= Weather.getForecast();
-    $scope.location= Weather.getLocation();
+
+    $scope.location = Weather.getLocation();
+
+    Weather.getCurrent.get(function(data){
+
+
+
+        $scope.currentWeather = data;
+        console.log(data);
+       // $scope.CurrentWeather.wind = data.wind.speed;
+        //  $scope.humidity = data.main.humidity;
+        //  $scope.precipitation = data.rain;
+        
+        //一句话描述
+        $scope.description= data.weather[0].main;
+        
+        //当前温度
+        $scope.temperature= Math.round(data.main.temp-273);
+
+        //图片
+        $scope.imgUrl = Weather.getWeatherImg(data.weather[0].main);
+
+       });
     
+
+
+
+     Weather.getForecast.get(function(data){
+
+      var result = data['HeWeather data service 3.0']
+      console.log(data['HeWeather data service 3.0']);
+      
+//今天
+      var todayWeather= result[0].daily_forecast[0];
+      
+      //气温
+      $scope.maxTempToday = todayWeather.tmp.max;
+      $scope.minTempToday = todayWeather.tmp.min;
+
+      //体感
+      $scope.flTempToday = result[0].now.cond.fl;
+
+      //图标相关
+      var iconIDToday = todayWeather.cond.code_d;
+      $scope.iconToday = Weather.getForeImg(iconIDToday);
+      //$scope.condToday = todayWeather.cond.text_d;
+
+      //日期
+      $scope.dateToday = todayWeather.date;
+      //湿度
+      $scope.humToday = todayWeather.hum;
+      //风力
+      $scope.windToday = todayWeather.wind.sc; 
+      //概率
+      $scope.precToday = todayWeather.pop;
+      //能见度
+      $scope.visToday =  todayWeather.vis;
+       
+
+
+//明天
+      var tmrWeather= result[0].daily_forecast[1];
+      //气温
+      $scope.maxTempTmr = tmrWeather.tmp.max;
+      $scope.minTempTmr = tmrWeather.tmp.min;
+      
+      //图标相关
+      var iconIDTmr = tmrWeather.cond.code_d;
+      $scope.iconTmr = Weather.getForeImg(iconIDTmr);
+      //$scope.condToday = todayWeather.cond.text_d;
+
+      //日期
+      $scope.dateTmr = tmrWeather.date;
+      //湿度
+      $scope.humTmr = tmrWeather.hum;
+      //风力
+      $scope.windTmr = tmrWeather.wind.sc; 
+      //概率
+      $scope.precTmr = tmrWeather.pop;
+      //能见度
+      $scope.visTmr =  tmrWeather.vis;
+
+//后天
+      var aftWeather= result[0].daily_forecast[2];
+      //气温
+      $scope.maxTempAft = aftWeather.tmp.max;
+      $scope.minTempAft = aftWeather.tmp.min;
+      
+      //图标相关
+      var iconIDAft = aftWeather.cond.code_d;
+      $scope.iconAft = Weather.getForeImg(iconIDAft);
+      //$scope.condToday = todayWeather.cond.text_d;
+
+      //日期
+      $scope.dateAft = aftWeather.date;
+      //湿度
+      $scope.humAft = aftWeather.hum;
+      //风力
+      $scope.windAft = aftWeather.wind.sc; 
+      //概率
+      $scope.precAft = aftWeather.pop;
+      //能见度
+      $scope.visAft =  aftWeather.vis;
+       
+
+       });
+
+
+
+
 
 
     $scope.toToday= function () {
@@ -51,27 +157,6 @@ weatherDressApp.controller('homeCtrl', function ($scope,Weather) {
     $scope.toFashion= function(){
       //scroll
     }
-
-
-
-
-
-
-    $scope.location = Weather.getLocation();
-
-    Weather.getCurrentWeather.get(function(data){
-
-
-
-        $scope.currentWeather = data;
-       // $scope.CurrentWeather.wind = data.wind.speed;
-        //  $scope.humidity = data.main.humidity;
-        //  $scope.temperature = data.main.temp;
-        //  $scope.precipitation = data.rain;
-        $scope.currentWeather.Img = Weather.getWeatherImg(data.weather.main);
-    });
-    
- 
 
 
 
--- app/js/wearModel.js
+++ app/js/wearModel.js
@@ -6,6 +6,7 @@
 
 weatherDressApp.factory('Weather',function ($resource,$cookieStore){
     var location = "Stockholm";
+    var country="Sweden"
     var APIKEY = "uid1600-33362460-67";
     var OpenWeatherAPI="aaf8a194828942ebcc29a98835489378"
    
@@ -14,22 +15,9 @@ weatherDressApp.factory('Weather',function ($resource,$cookieStore){
         return location;
     }
       
-
-    this.CurrentWeather  = $resource('http://api.openweathermap.org/data/2.5/weather?',{appid:'aaf8a194828942ebcc29a98835489378'});
-    this.Forecast = $resource('http://api.openweathermap.org/data/2.5/forecast?',{appid:'aaf8a194828942ebcc29a98835489378'});
-
-    //返回数据库，应用
-    this.getCurrentWeather = function () {
-       
-      this.Dish.get({id:inputID},function(data){
-          dish=data;
-         
-
-      });
-      return dish;
-    }
-
-
+    this.getCurrent = $resource('http://api.openweathermap.org/data/2.5/weather?',{q:location, APIKEY:'c3b7bba4b5ac511ec04d73ac4065ea83'});    
+    //this.getForecast = $resource('http://api.openweathermap.org/data/2.5//forecast?',{q:location, cnt:3,appid:'c3b7bba4b5ac511ec04d73ac4065ea83'});
+    this.getForecast = $resource('https://api.heweather.com/x3/weather?',{city:location, cnty:country,key:'939ca234771f43f29168f5e5d68257a5'});
 
 
 
@@ -43,7 +31,6 @@ weatherDressApp.factory('Weather',function ($resource,$cookieStore){
     this.getClothing = $resource('http://api.shopstyle.com/api/v2/products?',{fts:"dresses",pid:'uid1600-33362460-67'});
     this.getAccessories = $resource('http://api.shopstyle.com/api/v2/products?',{fts:"accessories",pid:'uid1600-33362460-67'});
     this.getShoes = $resource('http://api.shopstyle.com/api/v2/products?',{fts:"shoes",pid:'uid1600-33362460-67'});
-    this.getCurrentWeather = $resource('http://api.openweathermap.org/data/2.5/weather?',{q:location, APIKEY:'c3b7bba4b5ac511ec04d73ac4065ea83'});    
     
     //天气情况图片，需要更多，做成数组
      this.getWeatherImg = function(condition){
@@ -60,11 +47,71 @@ weatherDressApp.factory('Weather',function ($resource,$cookieStore){
          return url;
      }
     
+
+    this.getForeImg = function(id){
+        for (key in iconSet) {
+            if(iconSet[key].id==id){
+                return iconSet[key].url;
+            }
+        }
+    }
     
     
     
     
-    
+    var iconSet = [
+{"id":"100","cond":"Sunny/Clear","url":"http://files.heweather.com/cond_icon/100.png"},
+{"id":"101","cond":"Cloudy","url":"http://files.heweather.com/cond_icon/101.png"},
+{"id":"102","cond":"Few Clouds","url":"http://files.heweather.com/cond_icon/102.png"},
+{"id":"103","cond":"Partly Cloudy","url":"http://files.heweather.com/cond_icon/103.png"},
+{"id":"104","cond":"Overcast","url":"http://files.heweather.com/cond_icon/104.png"},
+{"id":"200","cond":"Windy","url":"http://files.heweather.com/cond_icon/200.png"},
+{"id":"201","cond":"Calm","url":"http://files.heweather.com/cond_icon/201.png"},
+{"id":"202","cond":"Light Breeze","url":"http://files.heweather.com/cond_icon/202.png"},
+{"id":"203","cond":"Moderate/Gentle Breeze","url":"http://files.heweather.com/cond_icon/203.png"},
+{"id":"204","cond":"Fresh Breeze","url":"http://files.heweather.com/cond_icon/204.png"},
+{"id":"205","cond":"Strong Breeze","url":"http://files.heweather.com/cond_icon/205.png"},
+{"id":"206","cond":"High Wind","url":" Near Gale","undefined":"http://files.heweather.com/cond_icon/206.png"},
+{"id":"207","cond":"Gale","url":"http://files.heweather.com/cond_icon/207.png"},
+{"id":"208","cond":"Strong Gale","url":"http://files.heweather.com/cond_icon/208.png"},
+{"id":"209","cond":"Storm","url":"http://files.heweather.com/cond_icon/209.png"},
+{"id":"210","cond":"Violent Storm","url":"http://files.heweather.com/cond_icon/210.png"},
+{"id":"211","cond":"Hurricane","url":"http://files.heweather.com/cond_icon/211.png"},
+{"id":"212","cond":"Tornado","url":"http://files.heweather.com/cond_icon/212.png"},
+{"id":"213","cond":"Tropical Storm","url":"http://files.heweather.com/cond_icon/213.png"},
+{"id":"300","cond":"Shower Rain","url":"http://files.heweather.com/cond_icon/300.png"},
+{"id":"301","cond":"Heavy Shower Rain","url":"http://files.heweather.com/cond_icon/301.png"},
+{"id":"302","cond":"Thundershower","url":"http://files.heweather.com/cond_icon/302.png"},
+{"id":"303","cond":"Heavy Thunderstorm","url":"http://files.heweather.com/cond_icon/303.png"},
+{"id":"304","cond":"Hail","url":"http://files.heweather.com/cond_icon/304.png"},
+{"id":"305","cond":"Light Rain","url":"http://files.heweather.com/cond_icon/305.png"},
+{"id":"306","cond":"Moderate Rain","url":"http://files.heweather.com/cond_icon/306.png"},
+{"id":"307","cond":"Heavy Rain","url":"http://files.heweather.com/cond_icon/307.png"},
+{"id":"308","cond":"Extreme Rain","url":"http://files.heweather.com/cond_icon/308.png"},
+{"id":"309","cond":"Drizzle Rain","url":"http://files.heweather.com/cond_icon/309.png"},
+{"id":"310","cond":"Storm","url":"http://files.heweather.com/cond_icon/310.png"},
+{"id":"311","cond":"Heavy Storm","url":"http://files.heweather.com/cond_icon/311.png"},
+{"id":"312","cond":"Severe Storm","url":"http://files.heweather.com/cond_icon/312.png"},
+{"id":"313","cond":"Freezing Rain","url":"http://files.heweather.com/cond_icon/313.png"},
+{"id":"400","cond":"Light Snow","url":"http://files.heweather.com/cond_icon/400.png"},
+{"id":"401","cond":"Moderate Snow","url":"http://files.heweather.com/cond_icon/401.png"},
+{"id":"402","cond":"Heavy Snow","url":"http://files.heweather.com/cond_icon/402.png"},
+{"id":"403","cond":"Snowstorm","url":"http://files.heweather.com/cond_icon/403.png"},
+{"id":"404","cond":"Sleet","url":"http://files.heweather.com/cond_icon/404.png"},
+{"id":"405","cond":"Rain And Snow","url":"http://files.heweather.com/cond_icon/405.png"},
+{"id":"406","cond":"Shower Snow","url":"http://files.heweather.com/cond_icon/406.png"},
+{"id":"407","cond":"Snow Flurry","url":"http://files.heweather.com/cond_icon/407.png"},
+{"id":"500 ","cond":"Mist","url":"http://files.heweather.com/cond_icon/500.png"},
+{"id":"501","cond":"Foggy","url":"http://files.heweather.com/cond_icon/501.png"},
+{"id":"502","cond":"Haze","url":"http://files.heweather.com/cond_icon/502.png"},
+{"id":"503","cond":"Sand","url":"http://files.heweather.com/cond_icon/503.png"},
+{"id":"504","cond":"Dust","url":"http://files.heweather.com/cond_icon/504.png"},
+{"id":"507","cond":"Duststorm","url":"http://files.heweather.com/cond_icon/507.png"},
+{"id":"508","cond":"Sandstorm","url":"http://files.heweather.com/cond_icon/508.png"},
+{"id":"900","cond":"Hot","url":"http://files.heweather.com/cond_icon/900.png"},
+{"id":"901","cond":"Cold","url":"http://files.heweather.com/cond_icon/901.png"},
+{"id":"999","cond":"Unknown","url":"http://files.heweather.com/cond_icon/999.png"}
+]
     return this;
 
  });
