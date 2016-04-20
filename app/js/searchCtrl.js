ada391f3cf7238a431bd4ea0186c01daf9c2abce
Christ1992 <yingjie@n180-p123.kthopen.kth.se>
2016-04-20 11:13:53 +0200

    fashion

--- app/js/searchCtrl.js
+++ app/js/searchCtrl.js
@@ -32,5 +32,26 @@ weatherDressApp.controller('searchCtrl', function ($scope,Weather) {
      }
     
     $scope.search("dressing");
+
+    /* When the user clicks on the button, 
+    toggle between hiding and showing the dropdown content */
+    $scope.myFunction=function(){
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+    // Close the dropdown menu if the user clicks outside of it
+    window.onclick = function(event) {
+      if (!event.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
+      }
+    }
  
 });
