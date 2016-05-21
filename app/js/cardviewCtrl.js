b7876fa62bf170e47ccaa25fc72524224dff0e59
Christ1992 <yingjie@YingjiedeMBP.lan>
2016-05-21 11:03:46 +0200

    add cardview title

--- app/js/cardviewCtrl.js
+++ app/js/cardviewCtrl.js
@@ -1,7 +1,14 @@
 weatherDressApp.controller('cardviewCtrl', function ($scope,$routeParams,$sce,Weather) {
-	$scope.genre = $routeParams.OutfitNum;
+	var genre = $routeParams.OutfitNum;
 	$scope.gender = $routeParams.Gender;
-  
+  	switch(genre){
+  		case 'genre1':$scope.genre= 'Casual'; break;
+  		case 'genre2':$scope.genre= 'Outdoor'; break;
+  		case 'genre3':$scope.genre= 'Office'; break;
+  		case 'genre4':$scope.genre= 'Fashion'; break;
+
+  	}
+  	
 
 
  });
